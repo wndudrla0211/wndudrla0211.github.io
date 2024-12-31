ac93f7a3c1dc2b8711a621d7761646230eabd875
wndudrla0211 <alex99.young.kim@gmail.com>
2024-12-31 02:47:09 -0500

    Delete js directory

--- js/script.js
+++
@@ -1,9 +0,0 @@
-// 스무스 스크롤 기능
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-      e.preventDefault();
-      document.querySelector(this.getAttribute('href')).scrollIntoView({
-          behavior: 'smooth'
-      });
-  });
-});
